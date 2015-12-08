8991fcce28150e0b3cad31a49c48b2e3f32611e9
Elek, Márton <elek@users.noreply.github.com>
2015-12-08 22:46:21 +0100

    new: initial import

---
+++ src/main/java/hu/tilos/radio/backend/UtilStarter.java
@@ -0,0 +1,95 @@
+package hu.tilos.radio.backend;
+
+import com.google.gson.Gson;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.matcher.Matchers;
+import com.mongodb.DB;
+import hu.radio.tilos.model.Role;
+import hu.tilos.radio.backend.spark.*;
+import hu.tilos.radio.backend.status.StatusService;
+import org.dozer.DozerBeanMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+import spark.ResponseTransformer;
+import spark.Route;
+
+import javax.validation.Validator;
+import java.util.List;
+
+import static spark.Spark.get;
+
+public class UtilStarter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UtilStarter.class);
+
+    private Gson gson;
+
+    static Injector injector;
+
+    private FiniteDuration timeout;
+
+    @Inject
+    StatusService statusService;
+
+    @Inject
+    @Configuration(name = "port.util")
+    private int portComment;
+
+    public static void main(String[] args) {
+        injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bindListener(Matchers.any(), new GuiceConfigurationListener());
+                bind(Integer.class).toInstance(1);
+
+
+            }
+        });
+        injector.getInstance(UtilStarter.class).run();
+
+    }
+
+    private void run() {
+        timeout = Duration.create(5, "seconds");
+
+        LOG.info("Starting new deployment");
+
+        SparkDefaults spark = new SparkDefaults(portComment, injector);
+
+        JsonTransformer jsonResponse = spark.getJsonTransformer();
+        get("/api/v1/status/radio", (req, res) -> statusService.getLiveSources(), jsonResponse);
+
+        get("/api/v1/status/radio.txt", (req, res) -> statusService.getLiveSources(), new ResponseTransformer() {
+            @Override
+            public String render(Object model) throws Exception {
+                StringBuilder result = new StringBuilder();
+                List<String> list = (List<String>) model;
+                for (String line : list) {
+                    result.append(line + "\n");
+                }
+                return result.toString();
+            }
+        });
+
+
+    }
+
+    private Route authorized(Role role, AuthorizedRoute authorizedRoute) {
+        Authorized authorized = new Authorized(role, authorizedRoute);
+        injector.injectMembers(authorized);
+        return authorized;
+    }
+
+    private Route authorized(String permission, AuthorizedRoute authorizedRoute) {
+        Authorized authorized = new Authorized(permission, authorizedRoute);
+        injector.injectMembers(authorized);
+        return authorized;
+    }
+
+
+}
